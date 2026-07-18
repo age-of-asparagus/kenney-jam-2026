45a22fbdfb58c9f5de641eb4bbbc1a4ca8d72aa6
Tylere Couture <tylere.couture@gmail.com>
2026-07-17 20:19:03 -0700

    main menu sounds

--- main_menu.gd
+++ main_menu.gd
@@ -1,6 +1,13 @@
 extends Control
 
 @export var GameScene : PackedScene
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var audio_stream_player_hover: AudioStreamPlayer = $AudioStreamPlayerHover
 
 func _on_start_pressed() -> void:
+	audio_stream_player.play()
+	await $AudioStreamPlayer.finished
 	get_tree().change_scene_to_packed(GameScene)
+
+func _on_start_mouse_entered() -> void:
+	audio_stream_player_hover.play()
