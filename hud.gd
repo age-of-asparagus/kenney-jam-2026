a0ea6f188e6a91bea2dbb3434647fa8cf87a05d3
Tylere Couture <tylere.couture@gmail.com>
2026-07-17 19:57:10 -0700

    smooth sliders

--- hud.gd
+++ hud.gd
@@ -2,17 +2,17 @@ extends Control
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _on_h_slider_1_value_changed(value: float) -> void:
-	audio_stream_player.play()
+	#audio_stream_player.play()
 	Global.size = value
 
 func _on_h_slider_2_value_changed(value: float) -> void:
-	audio_stream_player.play()
+	#audio_stream_player.play()
 	Global.speed = value
 
 func _on_h_slider_3_value_changed(value: float) -> void:
-	audio_stream_player.play()
+	#audio_stream_player.play()
 	Global.gravity = value
 
 func _on_h_slider_4_value_changed(value: float) -> void:
-	audio_stream_player.play()
+	#audio_stream_player.play()
 	Global.jump = value
