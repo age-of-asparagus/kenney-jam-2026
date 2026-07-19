470daf4e2794f6ca170ddf4dda3b1a0859381b10
Tylere Couture <tylere.couture@gmail.com>
2026-07-18 21:46:17 -0700

    Level selection (debug starting points)

--- main_menu.gd
+++ main_menu.gd
@@ -4,17 +4,42 @@ extends Control
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var audio_stream_player_hover: AudioStreamPlayer = $AudioStreamPlayerHover
 
-func _on_start_pressed() -> void:
-	Global.reset()
+func play_click():
 	audio_stream_player.play()
 	await $AudioStreamPlayer.finished
+	
+func play_game():
+	play_click()
 	get_tree().change_scene_to_packed(GameScene)
+
+func _on_start_pressed() -> void:
+	Global.reset()
+	play_game()
 
 func _on_start_mouse_entered() -> void:
 	audio_stream_player_hover.play()
 
-
 func _on_continue_pressed() -> void:
-	audio_stream_player.play()
-	await $AudioStreamPlayer.finished
-	get_tree().change_scene_to_packed(GameScene)
+	play_game()
+
+func _on_level_2_pressed() -> void:
+	Global.reset()
+	Global.sliders_activated[0]=true
+	Global.spawn = Vector2(4200, 100)
+	play_game()
+
+
+func _on_level_3_pressed() -> void:
+	Global.reset()
+	Global.sliders_activated[0]=true
+	Global.sliders_activated[1]=true
+	Global.spawn = Vector2(11500, 0)
+	play_game()
+
+
+func _on_level_4_pressed() -> void:
+	Global.reset()
+	Global.sliders_activated[1]=true
+	Global.sliders_activated[2]=true
+	Global.sliders_activated[1]=true
+	play_game()
