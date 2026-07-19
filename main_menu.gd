e6dbe82ced5db0cdc0b452224a95b4baad991132
Tylere Couture <tylere.couture@gmail.com>
2026-07-19 09:13:14 -0700

    merge

--- main_menu.gd
+++ main_menu.gd
@@ -17,28 +17,31 @@ extends Control
 
 
 
-func play_click():
-	audio_stream_player.play()
-	await $AudioStreamPlayer.finished
-	
 func play_game():
-	play_click()
+	$AudioStreamPlayer.play()
+	await $AudioStreamPlayer.finished
 	get_tree().change_scene_to_packed(GameScene)
 
 func _on_start_pressed() -> void:
 	Global.new_game_reset()
 	play_game()
 
-func _on_start_mouse_entered() -> void:
-	audio_stream_player_hover.play()
+#func _on_start_mouse_entered() -> void:
+	#audio_stream_player_hover.play()
 
 func _on_continue_pressed() -> void:
 	Global.continue_reset()
+	play_game()
+	
+func _on_level_1_pressed() -> void:
+	Global.continue_reset()
+	Global.sliders_activated[0]=true
 	play_game()
 
 func _on_level_2_pressed() -> void:
 	Global.continue_reset()
 	Global.sliders_activated[0]=true
+	Global.sliders_activated[1]=true
 	#Global.spawn = Vector2(6400, 100)
 	Global.spawn = Vector2(10496, 0)
 	play_game()
@@ -48,6 +51,7 @@ func _on_level_3_pressed() -> void:
 	Global.continue_reset()
 	Global.sliders_activated[0]=true
 	Global.sliders_activated[1]=true
+	Global.sliders_activated[2]=true
 	#Global.spawn = Vector2(113*64, 64)
 	Global.spawn = Vector2(16640, 640)
 	play_game()
@@ -55,9 +59,10 @@ func _on_level_3_pressed() -> void:
 
 func _on_level_4_pressed() -> void:
 	Global.continue_reset()
-	Global.sliders_activated[1]=true
-	Global.sliders_activated[3]=true
 	Global.sliders_activated[0]=true
+	Global.sliders_activated[1]=true
+	Global.sliders_activated[2]=true
+	Global.sliders_activated[3]=true
 	#Global.spawn = Vector2(396*64, 14*64)
 	Global.spawn = Vector2(34000, -150)
 	play_game()
