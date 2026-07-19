ff14ba89efed423865f2be6abb2cb4382181221e
Tylere Couture <tylere.couture@gmail.com>
2026-07-18 20:44:24 -0700

    duplicate level cus Kam too laxy

--- death_wall.gd
+++ death_wall.gd
@@ -3,7 +3,7 @@ extends Area2D
 @export var speed = 250
 
 func _ready():
-	global_position.x = get_parent().get_node("Player").global_position.x - 1000
+	global_position.x = get_parent().get_parent().get_node("Player").global_position.x - 1000
 
 func _physics_process(delta):
 	global_position.x += speed * delta
